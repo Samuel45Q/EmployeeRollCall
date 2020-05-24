9ef3067123cd020614c140a714d0dc4f7ae25fba
C613002 <lucas@glucode.com>
2020-05-24 22:36:44 +0200

    Clear LiveData Observers

--- App/EmployeeRollCallApplication/app/src/main/java/com/sam/employeerollcallapplication/activities/MainActivity.kt
+++ App/EmployeeRollCallApplication/app/src/main/java/com/sam/employeerollcallapplication/activities/MainActivity.kt
@@ -3,10 +3,12 @@ package com.sam.employeerollcallapplication.activities
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
 import com.sam.employeerollcallapplication.R
+import com.sam.employeerollcallapplication.models.LoginResponse
+import com.sam.employeerollcallapplication.utils.DialogUtil
+import com.sam.employeerollcallapplication.utils.Resource
 import com.sam.employeerollcallapplication.utils.ResourceStatus
 import com.sam.employeerollcallapplication.viewmodels.ViewModels
 import kotlinx.android.synthetic.main.activity_main.*
@@ -15,6 +17,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : BaseActivity() {
 
     private val viewModels: ViewModels by lazy { getViewModel(ViewModels::class.java) }
+    private var loginLiveData: LiveData<Resource<LoginResponse>>? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,9 +35,10 @@ class MainActivity : BaseActivity() {
 
     private fun setLoginOnClick() {
         btnClickLogin.setOnClickListener {
-            showProgressDialog("Logging in....")
             val emailText = username.text.toString()
             val textUsername = userPassword.text.toString()
+            removeObservers(emailText, textUsername)
+            showProgressDialog("Logging in....")
             if (emailText.isEmpty() || textUsername.isEmpty()) {
                 showErrorMessage("Failed to login")
                 showErrorMessage("Username/Password cannot be empty")
@@ -44,45 +48,50 @@ class MainActivity : BaseActivity() {
         }
     }
 
-    private fun setupViewModel(emailText: String, textUsername: String) {
-        viewModels.loginRequestLiveData(emailText, textUsername)
-            ?.observe(this, Observer { resource ->
-                when (resource.status) {
-                    ResourceStatus.SUCCESS -> {
-                        resource.data?.user.let {
-                            if (it!!.status) {
-                                hideProgressDialog()
-                                val sharedPreferences =
-                                    getSharedPreferences("user", Context.MODE_PRIVATE)
-                                sharedPreferences.edit().apply {
-                                    putString("userName", "${it.firstName} ${it.lastName}")
-                                    putInt("employeeId", it.id)
-                                    putString("role", it.role)
-                                    putInt("annualLeave", it.leaveBalance.annual)
-                                    putInt("sickLeave", it.leaveBalance.sick)
-                                    putInt("studyLeave", it.leaveBalance.study)
-                                    putInt("maternityLeave", it.leaveBalance.maternity)
-                                }.apply()
-                                startActivity(Intent(this@MainActivity, Dashboard::class.java))
-                            }
+    private fun setupViewModel(username: String, password: String) {
+        removeObservers(username, password)
+        loginLiveData = viewModels.loginRequestLiveData(username, password)
+        loginLiveData?.observe(this, Observer { resource ->
+            when (resource.status) {
+                ResourceStatus.SUCCESS -> {
+                    resource.data?.user.let {
+                        if (it!!.status) {
+                            hideProgressDialog()
+                            val sharedPreferences =
+                                getSharedPreferences("user", Context.MODE_PRIVATE)
+                            sharedPreferences.edit().apply {
+                                putString("userName", "${it.firstName} ${it.lastName}")
+                                putInt("employeeId", it.id)
+                                putString("role", it.role)
+                                putInt("annualLeave", it.leaveBalance.annual)
+                                putInt("sickLeave", it.leaveBalance.sick)
+                                putInt("studyLeave", it.leaveBalance.study)
+                                putInt("maternityLeave", it.leaveBalance.maternity)
+                            }.apply()
+                            startActivity(Intent(this@MainActivity, Dashboard::class.java))
                         }
                     }
-                    ResourceStatus.ERROR -> {
-                        hideProgressDialog()
-                        showErrorMessage("Failed to login")
-                    }
-                    ResourceStatus.LOADING -> Unit
                 }
-            })
-    }
-
-    private fun <T> LiveData<T>.observeOnce(lifecycleOwner: LifecycleOwner, observer: Observer<T>) {
-        observe(lifecycleOwner, object : Observer<T> {
-            override fun onChanged(t: T?) {
-                observer.onChanged(t)
-                removeObserver(this)
+                ResourceStatus.ERROR -> {
+                    if (loginLiveData!!.hasObservers()) {
+                        loginLiveData!!.removeObservers(this)
+                    }
+                    hideProgressDialog()
+                    showErrorMessage("Failed to login")
+                }
+                ResourceStatus.LOADING -> Unit
             }
         })
+    }
+
+    private fun removeObservers(userName: String, password: String) {
+        val observable: LiveData<Resource<LoginResponse>> =
+            viewModels.loginRequestLiveData(userName, password)!!
+        if (observable.hasObservers()
+        ) {
+            hideProgressDialog()
+            observable.removeObservers(this)
+        }
     }
 
     override fun onBackPressed() {
@@ -91,3 +100,4 @@ class MainActivity : BaseActivity() {
         this.finish()
     }
 }
+
